25e8ceb0719d206e731401cbc0e84646641eea49
ACMEproductions <acmeproductionsgm@gmail.com>
2020-11-23 22:33:55 -0600

    Process Monitor v1.1

--- ProcessMonitor.pl
+++ ProcessMonitor.pl
@@ -2,94 +2,81 @@ use strict;
 use warnings;
 use Win32::OLE('in');
 use Win32::Process::List;
-use Parse::Netstat qw(parse_netstat);
 
-#Variables globales
-my $processorCount;
-my $statsQuery;
-my $objWMIService = Win32::OLE->GetObject("winmgmts://./root/cimv2") or die "WMI connection failed.n";
-my $P = Win32::Process::List->new();
-my $res = parse_netstat(output=>join("", `netstat -anp`), flavor=>"win32");
-
-sub main()
-{
- my %list = $P->GetProcesses();
- foreach my $key ( keys %list ) {
-      my $process = substr $list{$key}, 0, -4;
-      #print $process."\n";
-      #$statsQuery="select * from Win32_PerfRawData_PerfProc_Process where IDProcess=".$$; #Usando PID
-      $statsQuery="select * from Win32_PerfRawData_PerfProc_Process where Name='".$process."'"; #Nombres de proceso sin .exe
-      #print $statsQuery;
-      GetProcessorCount();
-      GetUtilizationStats();
- }
- NetstatWin();
-}
-
-sub NetstatWin()
-{
-    my $res = parse_netstat(output=>join("", `netstat -anp`), flavor=>"win32");
-    print "$res\n";
-}
-
-sub GetProcessorCount()
-{
- my $cols=$objWMIService->ExecQuery("select NumberOfLogicalProcessors from Win32_ComputerSystem");
- 
- foreach my $v (in $cols) 
- { 
- $processorCount = $v->{NumberOfLogicalProcessors};
- }
-}
-
-sub GetUtilizationStats()
-{
- my $processingTime=0;
- my $timeStamp=0;
- my $cpu=0;
- my $cpumin=0;
- my $ram=0;
- my $name;
- my $limit = 10;        #Cantidad de calculos que realiza por proceso
- my $tolerancia = 10;   #Porcentaje de CPU que levanta alerta
- while($limit != 0)
- {
-
-    my $cols = $objWMIService->ExecQuery($statsQuery);
-
-    foreach my $Data( in $cols){
-    $name=$Data->{Name};
-    $cpumin = GetCorrectCPUData($processingTime,$Data->{PercentProcessorTime},$timeStamp,$Data->{TimeStamp_Sys100NS});
-    $processingTime=$Data->{PercentProcessorTime};
-    $timeStamp=$Data->{TimeStamp_Sys100NS};
-    $ram=$Data->{WorkingSetPrivate};
+#Variables globales / constructores del sistema
+my $processorCount;     #Para obtener los procesadores del sistema
+my $statsQuery;         #Para crear las queries al sig objeto
+my $objWMIService = Win32::OLE->GetObject("winmgmts://./root/cimv2") or die "WMI fallido\n";  #Se crea objeto del sistema
+my $P = Win32::Process::List->new() or die "Lista fallida\n";    #Se crea una lista de procesos
+# Funcion ´rincipal para el monitoreo de procesos. Se mete en un ciclo while para que se realice más d euna vez
+sub main(){
+    my %list = $P->GetProcesses();
+    foreach my $key ( keys %list ) 
+    {
+        my $process = substr $list{$key}, 0, -4; #print $process."\n";
+        #$statsQuery="select * from Win32_PerfRawData_PerfProc_Process where IDProcess=".$$; #Usando PID
+        $statsQuery="select * from Win32_PerfRawData_PerfProc_Process where Name='".$process."'"; #Nombres de proceso sin .exe #print $statsQuery;
+        ProcNums();
+        GetProcessInfo(10,10);
     }
-
- if ($cpumin > $cpu){
-     $cpu = $cpumin;
- }
- $limit -= 1;
- }
- if ($cpu > 10){
- print "Name :".$name.", MAXCPU:".sprintf("%.2f",$cpu)."%, RAM:".$ram."\n"; #Mensaje a escribir
- }
 }
+# Obtiene datos para el cálculo de datos
+sub ProcNums(){
+    my $logicalProcs = $objWMIService->ExecQuery("select NumberOfLogicalProcessors from Win32_ComputerSystem");     #Obteneos los procesadores lógicos
+    foreach my $logicalProc (in $logicalProcs)
+    { 
+        $processorCount = $logicalProc->{NumberOfLogicalProcessors};
+    }
+}
+# Obtiene los datos del proceso
+sub GetProcessInfo(){
+    my $limit = shift;        #Cantidad de calculos que realiza por proceso
+    my $tolerancia = shift;   #Porcentaje de CPU que levanta alerta
 
 
-sub GetCorrectCPUData()
-{
- #CPU utilization calculation logic. 
- my $oldProcessingData = shift;
- my $newProcessingData = shift;
- my $oldProcessingTime = shift;
- my $newProcessingTime = shift;
- 
- my $diffProcessingData = ($newProcessingData - $oldProcessingData);
- my $diffProcessingTime = ($newProcessingTime - $oldProcessingTime);
- 
- my $utilization=(($diffProcessingData/$diffProcessingTime)*100)/$processorCount;
- 
- return $utilization;
+    my $processingTime=0;
+    my $timeStamp=0;
+    my $cpu=0;
+    my $cpumin=0;
+    my $ram=0;
+    my $procName;
+
+    while($limit != 0)
+    {
+        #Se obtiene el volcado de l ainformación del proceso
+        my $procDump = $objWMIService->ExecQuery($statsQuery);
+        #Se obtienen los datos que nos competen del proceso
+        foreach my $procData( in $procDump){
+            $procName = $procData->{Name};
+            $cpumin = CPUutil($processingTime,$procData->{PercentProcessorTime},$timeStamp,$procData->{TimeStamp_Sys100NS});
+            $processingTime = $procData->{PercentProcessorTime};
+            $timeStamp = $procData->{TimeStamp_Sys100NS};
+            $ram = $procData->{WorkingSetPrivate};
+        }
+        #Se guarda el valor máximo en CPU (para detectar el minero) de las rondas
+        if ($cpumin > $cpu){
+            $cpu = $cpumin;
+        }
+        #Se pasa a la sig ronda
+        $limit -= 1;
+    }
+    # Se revisa si ha superado la tolerancia establecida
+    if ($cpu > $tolerancia){
+        print "Nombre de proceso :".$procName.", MaxCPU:".sprintf("%.4f",$cpu)."%, RAM:".$ram."\n"; #Mensaje a escribir
+    }
+}
+#Obtiene el dato correcto del uso de CPU
+sub CPUutil(){
+    #Se pasan los calores de los argumentos a las variables
+    my $oldProcData = shift;
+    my $newProcData = shift;
+    my $oldProcTime = shift;
+    my $newProcTime = shift;
+    #Se obtiene el valor de CPU
+    my $procData = ($newProcData - $oldProcData);
+    my $procTime = ($newProcTime - $oldProcTime);
+    my $util=(($procData/$procTime)*100)/$processorCount;
+    return $util;
 }
 
 #Corre la función del monitoreo
