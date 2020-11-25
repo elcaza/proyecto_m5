f136bc4ecc9be7078150dc089464fd95610fbd45
El Capitán <heneverloses@gmail.com>
2020-11-25 00:03:27 -0600

    funcion firewall detect ps1

--- powershell/check_firewall.ps1
+++ powershell/check_firewall.ps1
@@ -18,18 +18,24 @@ function check_reg_value {
 	$value = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey("$base_key",$env:COMPUTERNAME).OpenSubKey("$reg_key").GetValue("$reg_value");
 
 	if ($value -eq "0"){
-		echo "Firewall apagado";
+		#echo "Firewall apagado";
+		return 0;
 	} 
 	elseif ($method -eq "1"){
-		echo "Firewall prendido";
+		#echo "Firewall prendido";
+		return 1;
 	} else {
-		echo "Error";
+		#echo "Error";
+		return -10;
 	}
 }
 
-check_reg_value "LocalMachine" $reg_fw_key1 "EnableFirewall"
-check_reg_value "LocalMachine" $reg_fw_key2 "EnableFirewall"
-check_reg_value "LocalMachine" $reg_fw_key3 "EnableFirewall"
-check_reg_value "LocalMachine" $reg_fw_key4 "EnableFirewall"
-check_reg_value "LocalMachine" $reg_fw_key5 "EnableFirewall"
-check_reg_value "LocalMachine" $reg_fw_key6 "EnableFirewall"
+$reg_key1 = check_reg_value "LocalMachine" $reg_fw_key1 "EnableFirewall"
+$reg_key2 = check_reg_value "LocalMachine" $reg_fw_key2 "EnableFirewall"
+$reg_key3 = check_reg_value "LocalMachine" $reg_fw_key3 "EnableFirewall"
+$reg_key4 = check_reg_value "LocalMachine" $reg_fw_key4 "EnableFirewall"
+$reg_key5 = check_reg_value "LocalMachine" $reg_fw_key5 "EnableFirewall"
+$reg_key6 = check_reg_value "LocalMachine" $reg_fw_key6 "EnableFirewall"
+
+$ioc_fw = $reg_key1 + $reg_key2 + $reg_key3 + $reg_key4 +$reg_key5 + $reg_key6;
+echo $ioc_fw;
